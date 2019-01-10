a37ddb32b18112562791b655ae30f5bb8d00b47d
klyu-ironman <klyu@vt.edu>
2019-01-10 00:04:06 -0500

    added singleRun for start of task allocation

---
+++ matlabCode/taskAllocation/singleRun.m
@@ -0,0 +1,8 @@
+% singleRun.m
+% this file is to run a single instance of the provided code
+
+clc; clear all; close all; % house keeping
+
+
+data = readData('test1.txt') % gets data from '' file
+
